83af37b1f2f9dcef64490ad7d6964f1c1bff544b
AxuBlade <jan@macheta.net>
2012-12-31 02:19:18 +0100

    initial commit

---
+++ header.h
@@ -0,0 +1,12 @@
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
